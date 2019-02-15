5ea69a3f62730e8847e75cdbae1a597035f66f36
Dmitry Moshalkov <damsh@me.com>
2019-02-15 17:31:23 +0300

    Initial working release

---
+++ src/lightcontrol.h
@@ -0,0 +1,259 @@
+#ifndef LightControl_h
+#define LightControl_h
+
+// Arduino pin attached to driver pins
+#define WHITE_PIN 3
+#define WHITE2_PIN 5
+#define WHITE3_PIN 6
+
+
+#define NOCONTROLLER_MODE_PIN 17
+
+// Smooth stepping between the values
+#define STEP 1
+#define INTERVAL 3 // 10
+const int pwmIntervals = 255; //255;
+float R; // equation for dimming curve
+
+
+
+
+
+Bounce debouncer = Bounce();
+
+byte oldButtonVal;
+
+boolean whiteColor=false;
+
+
+long lastcolorencoderValue=0;
+unsigned long previousStatMillis = 0;
+byte bNoControllerMode = HIGH;
+// Stores the current color settings
+byte channels[3] = {WHITE_PIN,WHITE2_PIN,WHITE3_PIN};
+
+
+
+
+// stores dimming level
+byte dimming = 100;
+byte target_dimming = 100;
+
+
+
+// time tracking for updates
+unsigned long lastupdate = millis();
+
+
+// update knobs changed
+unsigned long lastKnobsChanged;
+boolean bNeedToSendUpdate = false;
+Bounce debouncernocontroller = Bounce();
+
+
+void initLights()
+{
+
+//TCCR2B = TCCR2B & B11111000 | B00000010;
+//TCCR2B = TCCR2B & B11111000 | B00000010;
+
+//D5-6
+TCCR0B = TCCR0B & B11111000 | B00000001;
+//TCCR0B = TCCR0B & B11111000 | B00000010;
+//D3
+TCCR2B = TCCR2B & B11111000 | B00000001;
+//D9
+//TCCR1B = TCCR1B & B11111000 | B00000001;
+
+
+  pinMode(KNOB_BUTTON_PIN, INPUT_PULLUP);
+  digitalWrite(KNOB_BUTTON_PIN, HIGH);
+  debouncer.attach(KNOB_BUTTON_PIN);
+  debouncer.interval(5);
+  oldButtonVal = debouncer.read();
+
+    pinMode(NOCONTROLLER_MODE_PIN,INPUT_PULLUP);
+    digitalWrite(NOCONTROLLER_MODE_PIN,HIGH);
+    debouncernocontroller.attach(NOCONTROLLER_MODE_PIN);
+    debouncernocontroller.interval(5);
+    bNoControllerMode = debouncernocontroller.read();
+
+
+        pinMode(WHITE_PIN, OUTPUT);
+        pinMode(WHITE2_PIN, OUTPUT);
+        pinMode(WHITE3_PIN, OUTPUT);
+
+// set up dimming
+  R = (pwmIntervals * log10(2))/(log10(255));
+
+
+
+
+EEPROM_readAnything(0, values);
+EEPROM_readAnything(4, target_values);
+
+bCurrentBrightness = target_values[0];
+
+lastencoderValue = target_values[0];
+
+}
+
+
+
+// this gets called every INTERVAL milliseconds and updates the current pwm levels for all colors
+void updateLights() {
+
+
+  // for each color
+  for (int v = 0; v < NUM_CHANNELS; v++) {
+
+    if (values[v] < target_values[v]) {
+      values[v] += STEP;
+      if (values[v] > target_values[v]) {
+        values[v] = target_values[v];
+      }
+    }
+
+    if (values[v] > target_values[v]) {
+      values[v] -= STEP;
+      if (values[v] < target_values[v]) {
+        values[v] = target_values[v];
+      }
+    }
+  }
+
+  // dimming
+  if (dimming < target_dimming) {
+    dimming += STEP;
+    if (dimming > target_dimming) {
+      dimming = target_dimming;
+    }
+  }
+  if (dimming > target_dimming) {
+    dimming -= STEP;
+    if (dimming < target_dimming) {
+      dimming = target_dimming;
+    }
+  }
+
+
+
+
+		    if (isOnTable) {
+		      analogWrite(channels[2], pow (2, (values[2] / R)) - 1);
+		    } else {
+		      analogWrite(channels[2], 0);
+		    }
+
+  // set actual pin values
+  for (int i = 0; i < NUM_CHANNELS-1; i++) {
+    if (isOn) {
+      analogWrite(channels[i], pow (2, (values[i] / R)) - 1);
+    } else {
+      analogWrite(channels[i], 0);
+      	    	EEPROM_writeAnything(0, values);
+      	    	EEPROM_writeAnything(4, target_values);
+    }
+  }
+
+
+
+
+}
+
+
+void checkButtonClick()
+{
+  debouncer.update();
+  byte buttonVal = debouncer.read();
+  byte newLevel = 0;
+  if (buttonVal != oldButtonVal && buttonVal == LOW) {
+
+  	if (isOn)
+  	{
+  		isOn = false;
+  		isOnTable = false;
+  	}
+  	else
+  	{
+      values[0]=map(0,0,100,0,255);
+      values[1]=map(0,0,100,0,255);
+      values[2]=map(0,0,100,0,255);
+  		isOn = true;
+  		isOnTable = true;
+  	}
+
+        sendDataToMQTT();
+
+      }
+
+
+  oldButtonVal = buttonVal;
+}
+
+
+void checkRotaryEncoder()
+{
+  long encoderValue = knob.read();
+
+  if (encoderValue != lastencoderValue)
+  {
+        #ifdef NDEBUG
+  			Serial.print("Encoder: ");
+  			Serial.println(encoderValue);
+        #endif
+
+  			lastencoderValue=encoderValue;
+
+
+ for (int i = 0; i < NUM_CHANNELS-1; i++)
+{
+    target_values[i] = map(lastencoderValue,0,100,0,255);
+}
+
+
+	lastKnobsChanged = millis();
+	bNeedToSendUpdate = true;
+
+
+	//isOn = true;
+
+
+  }
+
+  if (encoderValue > 100) {
+    encoderValue = 100;
+    knob.write(100);
+  } else if (encoderValue < 13) {
+    encoderValue = 13;
+    knob.write(13);
+  }
+
+  //bCurrentBrightness = encoderValue;
+
+}
+
+
+void updateBrightness()
+{
+
+ unsigned long currentTempMillis = millis();
+    if(((currentTempMillis - lastKnobsChanged ) > KNOBUPDATE_TIME) && bNeedToSendUpdate)
+
+	{
+
+			bNeedToSendUpdate = false;
+
+      bCurrentBrightness = lastencoderValue;
+
+        sendDataToMQTT();
+
+
+	}
+
+}
+
+
+
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,276 @@
+/*****************************************************************************************************************/
+// Подключение:
+// G - стол
+// R - первый канал подсветки раб зоны
+// B - второй канал подсветки рабочей зоны
+/*****************************************************************************************************************/
+//#define NDEBUG
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Ethernet.h>
+//#include <EthernetUdp.h>
+#include <avr/wdt.h>
+
+//#define MQTT_MAX_PACKET_SIZE 256
+
+//#include <ArduinoJson.h>  // https://github.com/bblanchon/ArduinoJson
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient
+
+#include <Encoder.h>
+#include <Bounce2.h>
+#include "EEPROMAnything.h"
+
+//#include <StandardCplusplus.h>
+
+#define LED_BLUE   A1
+#define LED_GREEN  A2
+#define LED_RED    A3
+#define ETHERNET_RESET_PIN  4
+
+byte mac[] = {
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};
+byte ip[] = { 10, 11, 13, 24 };
+byte gateway[] = { 10, 11, 13, 1 };
+byte subnet[] = { 255, 255, 255, 0 };
+
+
+String MQTTserver = "192.168.100.124";
+String MQTTuser = "openhabian";
+String MQTTpwd = "damp19ax";
+String MQTTtopic = "home/kitchen/ledstrip";
+String MQTTClientID = "kitchenLEDStrip";
+
+// tracks if the strip should be on of off
+boolean isOn = false;
+boolean isOnTable = false;
+byte bCurrentBrightness = 100;
+long lastencoderValue=0;
+
+#define NUM_CHANNELS 3 // how many channels, RGBW=4 RGB=3...
+
+byte target_values[3] = {100,100,50};
+byte values[3] = {100,100,50};
+
+#define KNOB_ENC_PIN_1 14    // Rotary encoder input pin 1 (A0)
+#define KNOB_ENC_PIN_2 7    // Rotary encoder input pin 2
+#define KNOB_BUTTON_PIN 8   // Rotary encoder button pin
+
+#define KNOBUPDATE_TIME 500
+
+Encoder knob(KNOB_ENC_PIN_2, KNOB_ENC_PIN_1);
+
+
+#include "services.h"
+#include "lightcontrol.h"
+
+#define MQTTUPDATEINTERVAL 3760000 //940000
+#define MQTTCHECKINTERVAL 840000 //940000
+
+
+
+long lastMQTTUpdate = 0;
+long lastMQTTCheckUpdate = 0;
+
+bool bDHCPError = false;
+
+void(* resetFunc) (void) = 0;
+
+void setup() {
+
+//Ethernet.init(10);
+
+  #ifdef NDEBUG
+   Serial.begin(9600);
+  #endif
+
+pinMode(LED_BLUE,OUTPUT);
+pinMode(LED_GREEN,OUTPUT);
+pinMode(LED_RED,OUTPUT);
+
+digitalWrite(LED_BLUE,LOW);
+digitalWrite(LED_GREEN,LOW);
+digitalWrite(LED_RED,LOW);
+
+delay(1000);
+
+
+     //if (Ethernet.linkStatus() == Unknown) {
+       //Serial.println("Link status unknown. Link status detection is only available with W5200 and W5500.");
+     //}
+     //else if (Ethernet.linkStatus() == LinkON) {
+       //Serial.println("Link status: On");
+     //}
+     //else if (Ethernet.linkStatus() == LinkOFF) {
+    //   Serial.println("Link status: Off");
+     //}
+
+//uint8_t retryCount = 0;
+
+//while ( ( Ethernet.linkStatus() != LinkON ) && retryCount < 21 )
+//{
+//  delay(1000);
+//  retryCount++;
+//}
+
+
+setColorState(2);
+delay(1000);
+setColorState(1);
+delay(1000);
+setColorState(0);
+delay(1000);
+setColorState(3);
+
+
+
+
+  //  Ethernet.begin(mac, ip);
+//  IPAddress ip(192,168,1,200);
+
+
+
+
+// Reset the W5500 module
+  pinMode(ETHERNET_RESET_PIN, OUTPUT);
+  digitalWrite(ETHERNET_RESET_PIN, HIGH);
+  digitalWrite(ETHERNET_RESET_PIN, LOW);
+  delay(800); //100
+  digitalWrite(ETHERNET_RESET_PIN, HIGH);
+  delay(800); //100
+
+if ( Ethernet.begin(mac) == 0)
+{
+
+  //Enable watchdog timer
+   wdt_enable(WDTO_8S);
+
+  setColorState(1);
+  delay(1000);
+  setColorState(0);
+  delay(1000);
+  setColorState(3);
+
+  while (true) {
+    delay(1);
+  }
+
+
+}
+
+   // print your local IP address:
+   //Serial.print("IP: ");
+   //Serial.println(Ethernet.localIP());
+
+   initLights();
+
+   setupMQTT();
+
+     setColorState(1);
+
+     //Enable watchdog timer
+     	wdt_enable(WDTO_8S);
+
+}
+
+void loop() {
+
+  switch (Ethernet.maintain()) {
+      case 1:
+        //renewed fail
+        //Serial.println("Error: renewed fail");
+             setColorState(2);
+             delay(1000);
+             while (true) {
+               delay(1);
+             }
+        break;
+
+      case 2:
+        //renewed success
+        //Serial.println("Renewed success");
+        //print your local IP address:
+        //Serial.print("My IP address: ");
+        //Serial.println(Ethernet.localIP());
+        break;
+
+      case 3:
+        //rebind fail
+        //Serial.println("Error: rebind fail");
+             setColorState(2);
+             delay(1000);
+             while (true) {
+               delay(1);
+             }
+        break;
+
+      case 4:
+        //rebind success
+        //Serial.println("Rebind success");
+        //print your local IP address:
+        //Serial.print("My IP address: ");
+        //Serial.println(Ethernet.localIP());
+        break;
+
+      default:
+        //nothing happened
+        break;
+    }
+
+
+    debouncernocontroller.update();
+    byte switchState = debouncernocontroller.read();
+
+
+
+    // and set the new light colors
+    if (millis() > lastupdate + INTERVAL) {
+      updateLights();
+      lastupdate = millis();
+    }
+
+  checkButtonClick();
+
+  checkRotaryEncoder();
+
+  updateBrightness();
+
+    /* Обработчик MQTT для режима "подписчика" */
+    mqttAPI.loop();
+
+
+    unsigned long currentCheckMQTTMillis = millis();
+
+    if( (currentCheckMQTTMillis - lastMQTTCheckUpdate ) > MQTTCHECKINTERVAL )
+    {
+       checkMQTT();
+       
+       if (mqttAPI.connected())
+       {
+         setColorState(0);
+       } else if (!bDHCPError)
+       {
+           setColorState(3);
+       }
+
+         lastMQTTCheckUpdate = currentCheckMQTTMillis;
+    }
+
+
+    unsigned long currentMQTTMillis = millis();
+
+       if( (currentMQTTMillis - lastMQTTUpdate ) > MQTTUPDATEINTERVAL )
+       {
+            sendDataToMQTT();
+
+
+         lastMQTTUpdate = currentMQTTMillis;
+       }
+
+    //MQTTtimer.run();
+
+    //reset watchdog timer
+    wdt_reset();
+
+}
